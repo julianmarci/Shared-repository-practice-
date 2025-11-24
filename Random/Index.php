08390e3ed6f6a0a030ca03112ee9aa9a5ffa533b
Facundo <facundo.moschion@gmail.com>
2025-11-24 17:19:57 +0100

    inicio

---
+++ Random/Index.php
@@ -0,0 +1,33 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php   
+    $random = rand(1, 3);
+    if ($random == 1){
+        echo ('');
+        elself ($random == 2)
+        echo('')
+        else
+}
+?>
+</head>
+<body>
+    <h1>
+        Css casuali
+    </h1>
+    <div>questo es un div dispari</div>
+    <div>questo es un div pari</div>
+    <div>questo es un div dispari</div>
+    <div>questo es un div pari</div>
+    <div>questo es un div dispari</div>
+    <div>questo es un div pari</div>
+    <div>questo es un div dispari</div>
+    <div>questo es un div pari</div>
+    <div>questo es un div dispari</div>
+    <div>questo es un div pari</div>
+    <div>questa è la fine della pagina</div>
+</body>
+</html>
