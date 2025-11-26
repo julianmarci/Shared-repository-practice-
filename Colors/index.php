15a0e53302b7751533315eab7cd4c88f5a2d686d
Facundo <facundo.moschion@gmail.com>
2025-11-26 16:56:58 +0100

    Link agregado

--- Colors/index.php
+++ Colors/index.php
@@ -10,6 +10,7 @@ $cantidad = rand(5, 30);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
