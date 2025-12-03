6f6bd4aa3f814b1a590270f6716c9bce83939d32
Facundo <facundo.moschion@gmail.com>
2025-12-03 16:11:20 +0100

    ad

---
+++ events/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Eventi Javascrpt</h1>
+    <button>testo rosso</button>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+    <div>Texto con sfondo giallo</div>
+</body>
+</html>
