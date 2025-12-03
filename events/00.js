0bfc8380461c944887789435bc13f11007a827db
julianmarci <julian.marci@hotmail.com>
2025-12-03 17:20:19 +0100

    Added small syntax corrections

--- events/00.js
+++ events/00.js
@@ -1,10 +1,7 @@
 //*whenever the user uses the button in Javascript, the yellow divs change to red
-
 var Button = document.getElementById("button");
-
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
     div[i].style.backgroundColor = "red";
-}
-});
+}});
