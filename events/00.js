365672426479877254eadda6a7e183ecf8fc39d0
julianmarci <julian.marci@hotmail.com>
2025-12-05 11:34:13 +0100

    Added corrections

--- events/00.js
+++ events/00.js
@@ -1,7 +1,21 @@
-//*whenever the user uses the button in Javascript, the yellow divs change to red
-var Button = document.getElementById("button");
+//*whenever the user uses the button in Javascript, the divs change to red
+var Button_red= document.getElementById("button");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
     div[i].style.backgroundColor = "red";
+}});
+//*whenever the user uses the button in Javascript, the divs change to blue
+var Button_blue = document.getElementById("button");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "blue";
+}});
+//*whenever the user uses the button in Javascript, the divs change to yellow
+var Button_yellow = document.getElementById("button");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "yellow";
 }});
