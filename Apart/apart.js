07ac402175c23d7b1de1b3b110f2d3e962f94166
julianmarci <julian.marci@hotmail.com>
2025-12-05 12:41:49 +0100

    Modified HTML and JS

---
+++ Apart/apart.js
@@ -0,0 +1,21 @@
+//*whenever the user uses the button in Javascript, the divs change to red
+var Button = document.getElementById("Button_red");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "red";
+}});
+//*whenever the user uses the button in Javascript, the divs change to blue
+var Button = document.getElementById("Button_blue");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "blue";
+}});
+//*whenever the user uses the button in Javascript, the divs change to yellow
+var Button = document.getElementById("Button_yellow");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "yellow";
+}});
--- events/00.js
+++ events/00.js
@@ -1,21 +1,16 @@
-//*whenever the user uses the button in Javascript, the divs change to red
-var Button = document.getElementById("Button_red");
+
+
+//*whenever the user uses the button in Javascript, the divs changes progressively to red, yeloow, then blue.
+var Button = document.getElementById("Button");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
-for (i = 0; i < div.length;i++){
-    div[i].style.backgroundColor = "red";
-}});
-//*whenever the user uses the button in Javascript, the divs change to blue
-var Button = document.getElementById("Button_blue");
-Button.addEventListener ('click', function() {
-var div = document.getElementsByTagName("div");
-for (i = 0; i < div.length;i++){
+for (i = 0; i < div.length;i+=3){
+    div[i].style.backgroundColor = "red"
+}
+for (i = 1; i < div.length;i+=3){
+    div[i].style.backgroundColor = "yellow"
+} 
+for (i = 2; i < div.length;i+=3){
     div[i].style.backgroundColor = "blue";
-}});
-//*whenever the user uses the button in Javascript, the divs change to yellow
-var Button = document.getElementById("Button_yellow");
-Button.addEventListener ('click', function() {
-var div = document.getElementsByTagName("div");
-for (i = 0; i < div.length;i++){
-    div[i].style.backgroundColor = "yellow";
+
 }});
