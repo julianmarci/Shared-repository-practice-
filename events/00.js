9bfffc1cce9c7707a9e78f7c58c3f99b829e255a
julianmarci <julian.marci@hotmail.com>
2025-12-05 11:46:54 +0100

    Added @GetElements

--- events/00.js
+++ events/00.js
@@ -1,19 +1,19 @@
 //*whenever the user uses the button in Javascript, the divs change to red
-var Button_red = document.getElementById("button");
+var Button_red = document.getElementById("button_red");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
     div[i].style.backgroundColor = "red";
 }});
 //*whenever the user uses the button in Javascript, the divs change to blue
-var Button_blue = document.getElementById("button");
+var Button_blue = document.getElementById("button_blue");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
     div[i].style.backgroundColor = "blue";
 }});
 //*whenever the user uses the button in Javascript, the divs change to yellow
-var Button_yellow = document.getElementById("button");
+var Button_yellow = document.getElementById("button_yellow");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
