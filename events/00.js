925b674faecf4ff342a068b9372daf8adcc1ae22
Facundo <facundo.moschion@gmail.com>
2025-12-05 12:11:30 +0100

    agregado el boton de alterna

--- events/00.js
+++ events/00.js
@@ -1,12 +1,13 @@
 //*whenever the user uses the button in Javascript, the divs change to red
-var Button = document.getElementById("Button_red");
+var Button = document.getElementById("Button");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
-    div[i].style.backgroundColor = "red";
+    div[i].style.backgroundColor = "red",
+    div[i+1].style.backgroundColor = "yellow";
 }});
 //*whenever the user uses the button in Javascript, the divs change to blue
-var Button = document.getElementById("Button_blue");
+/*var Button = document.getElementById("Button_blue");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
@@ -18,4 +19,4 @@ Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
     div[i].style.backgroundColor = "yellow";
-}});
+}});*/
