bd0eb96547aa91e12c63a4a9874b06859a5eab00
Facundo <facundo.moschion@gmail.com>
2025-12-15 11:30:31 +0100

    botones modificados

--- events/00.js
+++ events/00.js
@@ -7,7 +7,7 @@ for (i = 0; i < div.length;i++){
     div[i+1].style.backgroundColor = "yellow";
 }});
 //*whenever the user uses the button in Javascript, the divs change to blue
-/*var Button = document.getElementById("Button_blue");
+var Button = document.getElementById("Button_blue");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
