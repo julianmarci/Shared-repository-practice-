5074df1898de54f211330ec642fe21b458bf3b56
Facundo <facundo.moschion@gmail.com>
2025-12-15 12:06:04 +0100

    php en proceso

---
+++ groups/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <button id= "primo_rosso">
+            primo rosso
+        </button>
+        <button id="secondo_roso">
+            secondo roso
+        </button>
+        <button id="tutti_roso">
+            tutti roso
+        </button>
+        <button id= "primo_blu">
+            primo blu
+        </button>
+        <button id="secondo_blu">
+            secondo blu
+        </button>
+        <button id="tutti_blu">
+            tutti blu
+        </button>
+    </div>
+    <h2 class="primo">
+        Primo gruppo
+    </h2>
+    <?php
+        // Número aleatorio de divs entre 10 y 30
+        $cantidad = rand(5, 10);
+    ?>
+    <h2 class= "secondo">
+        Secondo gruppo
+    </h2>
+</body>
+</html>
