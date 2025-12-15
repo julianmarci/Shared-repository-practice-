35762265c139a8d3eb717fc32ea89894a7d7fefa
julianmarci <julian.marci@hotmail.com>
2025-12-15 12:57:54 +0100

    corrected syntax

--- events/00.js
+++ events/00.js
@@ -6,7 +6,7 @@ for (i = 0; i < div.length;i++){
     div[i].style.backgroundColor = "red";
 }});
 //*whenever the user uses the button in Javascript, the divs change to yellow
-var Button = document.getElementById("Button_yelllow");
+var Button = document.getElementById("Button_yellow");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
