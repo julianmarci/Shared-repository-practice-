aac225e6f4254be6005f2ab0e4c9334293934131
julianmarci <julian.marci@hotmail.com>
2025-12-15 11:49:14 +0100

    modified index and js to add more buttons and colors

--- events/00.js
+++ events/00.js
@@ -1,7 +1,35 @@
-//*whenever the user uses the button in Javascript, the yellow divs change to red
-var Button = document.getElementById("button");
+//*whenever the user uses the button in Javascript, the divs change to red
+var Button = document.getElementById("Button_red");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
     div[i].style.backgroundColor = "red";
+}});
+//*whenever the user uses the button in Javascript, the divs change to yellow
+var Button = document.getElementById("Button_yelllow");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "yellow";
+}});
+//*whenever the user uses the button in Javascript, the divs change to blue
+var Button = document.getElementById("Button_blue");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "blue";
+}});
+//*whenever the user uses the button in Javascript, the divs change to orange
+var Button = document.getElementById("Button_orange");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "orange";
+}});
+//*whenever the user uses the button in Javascript, the divs change to green
+var Button = document.getElementById("Button_green");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "green";
 }});
--- events/index.php
+++ events/index.php
@@ -8,7 +8,12 @@
 </head>
 <body>
     <h1>Eventi Javascrpt</h1>
-    <button id="button">testo rosso</button>
+    <button id="Button_red">testo rosso</button>
+    <button id="Button_yellow">testo giallo</button>
+    <button id="Button_blue">testo Blu</button>
+    <button id="Button_orange">testo Arancione</button>
+    <button id="Button_green">testo verde</button>
+    <button id="alterna">Alterna</button>
     <div>Texto con sfondo giallo</div>
     <div>Texto con sfondo giallo</div>
     <div>Texto con sfondo giallo</div>
