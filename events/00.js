49b16215a9f4ab7002880b3c266cca6e9269a1e0
julianmarci <julian.marci@hotmail.com>
2025-12-15 11:38:16 +0100

    modified variables and ordered logic

--- events/00.js
+++ events/00.js
@@ -1,9 +1,23 @@
-//*whenever the user uses the button in Javascript, the divs change to green
-var Button = document.getElementById("Button_green");
+//*whenever the user uses the button in Javascript, the divs change to red
+var Button = document.getElementById("Button_red");
 Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
-    div[i].style.backgroundColor = "green";
+    div[i].style.backgroundColor = "red";
+}});
+//*whenever the user uses the button in Javascript, the divs change to yellow
+var Button = document.getElementById("Button_yelllow");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "yellow";
+}});
+//*whenever the user uses the button in Javascript, the divs change to blue
+var Button = document.getElementById("Button_blue");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "blue";
 }});
 //*whenever the user uses the button in Javascript, the divs change to orange
 var Button = document.getElementById("Button_orange");
@@ -11,4 +25,11 @@ Button.addEventListener ('click', function() {
 var div = document.getElementsByTagName("div");
 for (i = 0; i < div.length;i++){
     div[i].style.backgroundColor = "orange";
+}});
+//*whenever the user uses the button in Javascript, the divs change to green
+var Button = document.getElementById("Button_green");
+Button.addEventListener ('click', function() {
+var div = document.getElementsByTagName("div");
+for (i = 0; i < div.length;i++){
+    div[i].style.backgroundColor = "green";
 }});
