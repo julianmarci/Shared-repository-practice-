247d59c84279f50a5ddd89d360e803380123cc2d
julianmarci <julian.marci@hotmail.com>
2025-12-17 11:52:44 +0100

    modified CSS and Javascript files to include the "Group 3" variables.

--- groups/00.js
+++ groups/00.js
@@ -8,6 +8,18 @@ myButton.addEventListener("click", function() {
     change_divs_color("group_2", "red");
 });
 
+myButton = document.getElementById("btn_red_3");
+myButton.addEventListener("click", function() {
+    change_divs_color("group_3", "red");
+});
+
+myButton = document.getElementById("btn_red_all");
+myButton.addEventListener("click", function() {
+    change_divs_color("group_1", "red");
+    change_divs_color("group_2", "red");
+    change_divs_color("group_3", "red");
+});
+
 myButton = document.getElementById("btn_blue_1");
 myButton.addEventListener("click", function() {
     change_divs_color("group_1", "blue");
@@ -18,16 +30,38 @@ myButton.addEventListener("click", function() {
     change_divs_color("group_2", "blue");
 });
 
+myButton = document.getElementById("btn_blue_3");
+myButton.addEventListener("click", function() {
+    change_divs_color("group_3", "blue");
+});
+
 myButton = document.getElementById("btn_blue_all");
 myButton.addEventListener("click", function() {
     change_divs_color("group_1", "blue");
     change_divs_color("group_2", "blue");
+    change_divs_color("group_3", "blue");
 });
 
-myButton = document.getElementById("btn_red_all");
+myButton = document.getElementById("btn_yellow_1");
 myButton.addEventListener("click", function() {
-    change_divs_color("group_1", "red");
-    change_divs_color("group_2", "red");
+    change_divs_color("group_1", "yellow");
+});
+
+myButton = document.getElementById("btn_yellow_2");
+myButton.addEventListener("click", function() {
+    change_divs_color("group_2", "yellow");
+});
+
+myButton = document.getElementById("btn_yellow_3");
+myButton.addEventListener("click", function() {
+    change_divs_color("group_3", "yellow");
+});
+
+myButton = document.getElementById("btn_yellow_all");
+myButton.addEventListener("click", function() {
+    change_divs_color("group_1", "yellow");
+    change_divs_color("group_2", "yellow");
+    change_divs_color("group_3", "yellow");
 });
 
 function change_divs_color(div_class, bg_color) {
