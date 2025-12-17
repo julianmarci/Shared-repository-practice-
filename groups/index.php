b24d3840388332cdd68a137e0c70cd893762121a
julianmarci <julian.marci@hotmail.com>
2025-12-17 11:59:29 +0100

    changed variable names

--- groups/index.php
+++ groups/index.php
@@ -12,20 +12,21 @@
     <div class="buttons">
         <button id="btn_red_1">Red 1</button>
         <button id="btn_blue_1">Blue 1</button>
-        <button id="primo_giallo">Primo Giallo</button>
+        <button id="btn_yellow_1">Yellow 1</button>
 
         <button id="btn_red_2">Red 2</button>
         <button id="btn_blue_2">Blue 2</button>
-        <button id="secondo_giallo">Secondo Giallo</button>
+        <button id="btn_yellow_2">Yellow 2</button>
+
+        <button id="btn_red_3">Red 3</button>
+        <button id="btn_blue_3">Blue 3</button>
+        <button id="btn_yellow_3">Yellow 3</button>
 
         <button id="btn_red_all">All Red</button>
         <button id="btn_blue_all">All Blue</button>
+        <button id="btn_yellow_all">All Yellow</button>
 
-        <button id="terzo_rosso">Terzo Rosso</button>
-        <button id="terzo_blu">Terzo Blu</button>
-        <button id="terzo_giallo">Terzo Giallo</button>
-
-        <button id="tutti_giallo">Tutti Giallo</button>
+        
 
     </div>
     <h2>
