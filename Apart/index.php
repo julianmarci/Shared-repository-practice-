a494356015554aafdb9c8fa28bbd4ac07c437f83
julianmarci <julian.marci@hotmail.com>
2026-01-13 12:35:41 +0100

    Added PHP to Apart folder in order to pull original documentation

--- Apart/index.php
+++ Apart/index.php
@@ -1,10 +1,29 @@
-<?php
-for ($i = 1; $i <= $cantidad; $i++) {
-    if ($i % 2 == 0) {
-        echo "<div>Div $i: </div>";
-    } else {
-        echo "<div>Div $i: </div>";
-    }
-}
+ <?php   
+    $nome = $_POST['nome'];
+    $cognome = $_POST['cognome'];
+    $residenza = $_POST['residenza'];
+    $importo = $_POST ['importo'];
+    $caparra = $_POST ['caparra'];
+    $saldo = $importo - $caparra;
+    echo ("<p>Nome: " . $nome . "</p>");
+    echo ("<p>Cognome: " . $cognome . "</p>");
+    echo ("<p>Residenza: " . $residenza . "</p>");
+    echo ("<p>Importo: " . $importo . "</p>");
+    echo ("<p>Saldo: " . $saldo . "</p>");
+    ?>
 
-
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Prenotazioni
+    </h1>
+    <h2>
+        Data di Arrivo
+    </h2>
+</body>
+</html>
